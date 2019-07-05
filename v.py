1440e13fe0b875a42d9665127422fa4726c0924e
meghana2812 <52518912+meghana2812@users.noreply.github.com>
2019-07-05 16:06:46 +0530

    Create v.py

---
+++ v.py
@@ -0,0 +1,7 @@
+def vowel_or_consonant(ch):
+if (ch == ‘a’ or ch == ‘e’ or ch == ‘i’ or ch == ‘o’ or ch == ‘u’ or ch == ‘A’ or ch == ‘E’ or ch == ‘I’ or ch == ‘O’ or ch == ‘U’):
+  print(“Vowel”)
+else:
+  print(“Consonant”)
+ch = input(“Enter a character : “)
+vowel_or_consonant(ch)
