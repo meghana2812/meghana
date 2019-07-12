9860dae3fa68db98b705b6fca7f6b7aed0bd950a
meghana2812 <52518912+meghana2812@users.noreply.github.com>
2019-07-12 15:25:13 +0530

    Update hun75.py

--- hun75.py
+++ hun75.py
@@ -2,7 +2,7 @@ sm = int(input())
 lr = list(map(int,input().split()))
 for i in range(len(lr)-1):
     if lr[i]>lr[i+1]:
-        print(lr[i+1])
+        print(lr[i+1],end=" ")
     else:
-        print(-1)
+        print(-1,end=" ")
 print(-1)
